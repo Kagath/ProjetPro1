1f110b6477683d1a86091fa24e1a5bcb1e57a8ee
Kagath <ericbouchi@live.fr>
2023-05-04 05:50:41 +0200

    Update check_email_leak.py

--- check_email_leak.py
+++ check_email_leak.py
@@ -6,7 +6,9 @@ from logger import log_info, log_error, save_result
 api_key = "59918da982474e6d99b3ac573ec6a02d"
 
 def run_check_email_leak():
-    email = input("Entrez l'adresse e-mail à vérifier : ")
+    print("\n________________________________________________________")
+    email = input("Entrez l'adresse e-mail à vérifier : \n")
+    print("\n")    
     print("Buvez un café, ça risque de prendre du temps..")
     print("Démarrage du scan de fuite de données sur cette adresse email.")
 
@@ -22,11 +24,12 @@ def run_check_email_leak():
             breaches = json.loads(response.content)
             log_info(f"Vérification des fuites de données pour l'adresse e-mail : {email}")
             save_result(f"Nombre de fuites trouvées pour {email} : {len(breaches)}", result_file="results/email_leak_results.txt")
-            print(f"\nBreaches found for {email}:")
+            print(f"\n{len(breaches)} fuites de données trouvées pour {email}: \n")
             for breach in breaches:
                 print(f"{breach['Name']} - {breach['Domain']}")
         elif response.status_code == 404:
-            log_info(f"Aucune fuite de données trouvée pour l'adresse e-mail : {email}")
+            log_info(f"Aucune fuite de données trouvée pour l'adresse e-mail : {email} \n")
+            print(f"Aucune fuite de données trouvée pour l'adresse e-mail : {email} \n")
         else:
             log_error(f"Erreur lors de la récupération des données de fuites pour l'adresse e-mail : {email} (code {response.status_code})")
     except Exception as e:
