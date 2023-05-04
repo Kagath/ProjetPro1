6841fa86d00c17c40278d66e409f542a76b3ae18
Kagath <ericbouchi@live.fr>
2023-05-04 05:26:33 +0200

    Update check_email_leak.py

--- check_email_leak.py
+++ check_email_leak.py
@@ -1,27 +1,33 @@
 import requests
+import json
 
 from logger import log_info, log_error, save_result
 
-def run_email_leak_scan():
-    email = input("Entrez l'adresse e-mail à vérifier : ")
-    log_info(f"Vérification des fuites d'email pour : {email}")
+api_key = "59918da982474e6d99b3ac573ec6a02d"
 
-    # Remplacez [your_key] par votre clé d'API personnelle pour Have I Been Pwned
-    api_key = "[your_key]"
+def run_check_email_leak():
+    email = input("Entrez l'adresse e-mail à vérifier : ")
+    print("Buvez un café, ça risque de prendre du temps..")
+    print("Démarrage du scan de fuite de données sur cette adresse email.")
+
+    base_url = f"https://haveibeenpwned.com/api/v3/breachedaccount/{email}"
     headers = {
         "hibp-api-key": api_key,
-        "user-agent": "MySecurityTool"
+        "User-Agent": "MyApp"
     }
-    url = f"https://haveibeenpwned.com/api/v3/breachedaccount/{email}"
 
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        breaches = response.json()
-        log_info(f"{email} a été trouvé dans {len(breaches)} fuites de données")
-        save_result(f"{email} a été trouvé dans {len(breaches)} fuites de données", result_file="results/email_leak_results.txt")
-    elif response.status_code == 404:
-        log_info(f"Aucune fuite de données trouvée pour {email}")
-        save_result(f"Aucune fuite de données trouvée pour {email}", result_file="results/email_leak_results.txt")
-    else:
-        log_error(f"Erreur lors de la vérification des fuites d'email pour {email} (code {response.status_code})")
+    try:
+        response = requests.get(base_url, headers=headers)
+        if response.status_code == 200:
+            breaches = json.loads(response.content)
+            log_info(f"Vérification des fuites de données pour l'adresse e-mail : {email}")
+            save_result(f"Nombre de fuites trouvées pour {email} : {len(breaches)}", result_file="results/email_leak_results.txt")
+            print(f"\nBreaches found for {email}:")
+            for breach in breaches:
+                print(f"{breach['Name']} - {breach['Domain']}")
+        elif response.status_code == 404:
+            log_info(f"Aucune fuite de données trouvée pour l'adresse e-mail : {email}")
+        else:
+            log_error(f"Erreur lors de la récupération des données de fuites pour l'adresse e-mail : {email} (code {response.status_code})")
+    except Exception as e:
+        log_error(f"Erreur lors de la vérification des fuites de données pour l'adresse e-mail : {email} : {e}")
