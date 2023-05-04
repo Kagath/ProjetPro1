ea4f47861de26eaa1a3e1840568e96055171cd22
Kagath <ericbouchi@live.fr>
2023-05-04 04:13:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from nmap_scan import run_nmap_scan
 from scrape_nvd import run_nvd_scrape
 from check_email_leak import run_email_leak_scan
 from dashboard import generate_dashboard
+from dashboard import clear_logs_and_results
 
 def create_directory(directory):
     if not os.path.exists(directory):
@@ -20,9 +21,10 @@ def main():
         print("Veuillez choisir une option:")
         print("1. Exécuter un scan Nmap")
         print("2. Exécuter une simulation d'attaque avec Metasploit")
-        print("3. Récupérer les vulnérabilités critiques à jour (NVD)")
+        print("3. Récupérer les vulnérabilités critiques à jour")
         print("4. Vérifier les fuites d'email")
         print("5. Générer un tableau de bord de sécurité")
+        print("6. Effacer tous les fichiers de résultats et de journaux")
         print("0. Quitter")
 
         choice = input("Entrez le numéro de votre choix: ")
@@ -39,7 +41,10 @@ def main():
             email = input("Entrez l'adresse e-mail à vérifier pour les fuites de données: ")
             check_email_leak(email)
         elif choice == "5":
-            dashboard()
+            generate_dashboard()
+        elif choice == "6":
+            clear_logs_and_results()
+            
         elif choice == "0":
             print("Au revoir!")
             sys.exit(0)
