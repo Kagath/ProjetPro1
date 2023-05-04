3275d4fc952509a893fd8b6c4aa67e84e02795fe
Kagath <ericbouchi@live.fr>
2023-05-04 03:24:58 +0200

    Update nmap_scan.py

--- nmap_scan.py
+++ nmap_scan.py
@@ -1,21 +1,25 @@
 import nmap
+from logger import log_info, log_error, save_result
 
-def run_nmap_scan(targets, options='-sV'):
-    nm = nmap.PortScanner()
-    nm.scan(hosts=targets, arguments=options)
+def run_nmap_scan():
+    targets = input("Veuillez entrer l'adresse IP, la plage d'adresses IP ou le CIDR à scanner (exemple : 192.168.1.0/24 ou 192.168.1.1-192.168.1.254) : ")
+    ports = "22,21,23,443,80,53,135,8080,8888" # Ports à scanner
+    options = "-A -O -sV -T4" # Options pour le scan Nmap
+    print("Buvez un cafe, ça risque de prendre du temps..")    
+    print("Démarrage du scan Nmap sur les cibles : {targets}, ports : {ports}")
+    log_info(f"Démarrage du scan Nmap sur les cibles : {targets}, ports : {ports}")
+    scanner = nmap.PortScanner()
 
-    for host in nm.all_hosts():
-        print(f"Host : {host} ({nm[host].hostname()})")
-        print("State : {}".format(nm[host].state()))
-        for protocol in nm[host].all_protocols():
-            print("Protocol : {}".format(protocol))
-            ports = nm[host][protocol].keys()
-            for port in ports:
-                print("port : {}\tstate : {}".format(port, nm[host][protocol][port]["state"]))
+    try:
+        scan_result = scanner.scan(hosts=targets, ports=ports, arguments=options)
+        log_info(f"Scan Nmap terminé pour les cibles : {targets}, ports : {ports}")
 
-    return nm
+        result_str = f"Résultats du scan Nmap pour les cibles : {targets}, ports : {ports}\n"
+        for host, host_info in scan_result["scan"].items():
+            result_str += f"{host}:\n"
+            for proto in host_info["tcp"]:
+                result_str += f"  {proto}: {host_info['tcp'][proto]['state']}\n"
+        save_result(result_str, result_file="results/nmap_results.txt")
 
-# Exemple d'utilisation de la fonction
-if __name__ == '__main__':
-    targets = '192.168.1.1-255'  # Exemple de cibles: une plage d'adresses IP
-    scan_results = run_nmap_scan(targets)
+    except Exception as e:
+        log_error(f"Erreur lors de l'exécution du scan Nmap : {e}")
