2aa19caff8e37975c87efd3271ce5a5274975f58
Kagath <ericbouchi@live.fr>
2023-05-05 04:02:30 +0200

    Update scrape_nvd.py

--- scrape_nvd.py
+++ scrape_nvd.py
@@ -1,23 +1,51 @@
 import requests
+import csv
+from datetime import datetime, timedelta
 
-from bs4 import BeautifulSoup
-from logger import log_info, log_error, save_result
+def scrape_nvd_vulnerabilities():
+    base_url = "https://services.nvd.nist.gov/rest/json/cves/1.0"
 
-def run_nvd_scrape():
-    base_url = "https://nvd.nist.gov/vuln/full-listing"
-    log_info(f"Début du scrapping des vulnérabilités critiques du NVD")
+    today = datetime.now()
+    yesterday = today - timedelta(days=1)
+    
+    today_str = today.strftime('%Y-%m-%dT00:00:00:000 UTC-05:00')
+    yesterday_str = yesterday.strftime('%Y-%m-%dT00:00:00:000 UTC-05:00')
 
-    response = requests.get(base_url)
+    params = {
+        "modStartDate": yesterday_str,
+        "modEndDate": today_str,
+        "resultsPerPage": 2000
+    }
+
+    response = requests.get(base_url, params=params)
 
     if response.status_code == 200:
-        soup = BeautifulSoup(response.content, "html.parser")
-        critical_vulns = soup.find_all("span", class_="red bold")
+        json_data = response.json()
+        vulnerabilities = json_data["result"]["CVE_Items"]
 
-        log_info(f"Scrapping des vulnérabilités critiques du NVD terminé")
-        save_result(f"Vulnérabilités critiques trouvées : {len(critical_vulns)}", result_file="results/nvd_results.txt")
+        with open('results/vulnerabilities.csv', 'w', newline='', encoding='utf-8') as csv_file, open('results/vulnerabilities.txt', 'w', encoding='utf-8') as txt_file:
+            fieldnames = ['CVE ID', 'Published Date', 'Last Modified Date', 'Description']
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            writer.writeheader()
+
+            for vulnerability in vulnerabilities:
+                cve_id = vulnerability["cve"]["CVE_data_meta"]["ID"]
+                published_date = vulnerability["publishedDate"]
+                last_modified_date = vulnerability["lastModifiedDate"]
+                description = vulnerability["cve"]["description"]["description_data"][0]["value"]
+
+                writer.writerow({
+                    'CVE ID': cve_id,
+                    'Published Date': published_date,
+                    'Last Modified Date': last_modified_date,
+                    'Description': description
+                })
+
+                txt_file.write(f"CVE ID: {cve_id}\n")
+                txt_file.write(f"Published Date: {published_date}\n")
+                txt_file.write(f"Last Modified Date: {last_modified_date}\n")
+                txt_file.write(f"Description: {description}\n")
+                txt_file.write("\n")
+
     else:
-        log_error(f"Erreur lors de la récupération des données du NVD (code {response.status_code})")
-
-# Pour tester la fonction run_nvd_scrape() directement
-if __name__ == "__main__":
-    run_nvd_scrape()
+        print(f"Error: Unable to fetch data (HTTP {response.status_code})")
