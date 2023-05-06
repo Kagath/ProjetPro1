d530797246e67ebd4ea5c9366784b6aaf1eb8596
Kagath <ericbouchi@live.fr>
2023-05-06 03:33:06 +0200

    Update dig.py

--- dig.py
+++ dig.py
@@ -1,5 +1,16 @@
 import subprocess
+
 from logger import log_info, log_error, save_result
+
+def run_dns_query(record_type, target_domain):
+    dig_command = f"dig {target_domain} {record_type} +noall +answer"
+    try:
+        output = subprocess.check_output(dig_command, shell=True, text=True)
+        log_info(f"Récupération des enregistrements {record_type} pour {target_domain}")
+        return output
+    except subprocess.CalledProcessError as e:
+        log_error(f"Erreur lors de la récupération des enregistrements {record_type} pour {target_domain}: {e}")
+        return ""
 
 def run_dns_enum():
     target_domain = input("\nEntrez le domaine cible pour l'énumération DNS: \n")
@@ -7,24 +18,18 @@ def run_dns_enum():
     log_info(f"Début de l'énumération DNS pour {target_domain}")
     print("\nDébut de l'énumération DNS \n")
 
-    try:
-        # Récupération des enregistrements A
-        dig_a_command = f"dig {target_domain} A +noall +answer"
-        a_records_output = subprocess.check_output(dig_a_command, shell=True, text=True)
-        save_result(a_records_output, result_file="results/dns_results.txt")
+    dns_records = {
+        "A": run_dns_query("A", target_domain),
+        "NS": run_dns_query("NS", target_domain),
+        "MX": run_dns_query("MX", target_domain),
+    }
 
-        # Récupération des enregistrements NS
-        dig_ns_command = f"dig {target_domain} NS +noall +answer"
-        ns_records_output = subprocess.check_output(dig_ns_command, shell=True, text=True)
-        save_result(ns_records_output, result_file="results/dns_results.txt")
+    with open("results/dns_results.txt", "w") as dns_results_file:
+        for record_type, records in dns_records.items():
+            dns_results_file.write(f"Enregistrements {record_type}:\n")
+            dns_results_file.write("------------------\n")
+            dns_results_file.write(records)
+            dns_results_file.write("\n")
 
-        # Récupération des enregistrements MX
-        dig_mx_command = f"dig {target_domain} MX +noall +answer"
-        mx_records_output = subprocess.check_output(dig_mx_command, shell=True, text=True)
-        save_result(mx_records_output, result_file="results/dns_results.txt")
-
-        log_info(f"Énumération DNS terminée pour {target_domain}")
-        print("\nÉnumération DNS terminée \n")
-
-    except subprocess.CalledProcessError as e:
-        log_error(f"Erreur lors de l'énumération DNS pour {target_domain}: {e}")
+    log_info(f"Énumération DNS terminée pour {target_domain}")
+    print("\nÉnumération DNS terminée \n")
